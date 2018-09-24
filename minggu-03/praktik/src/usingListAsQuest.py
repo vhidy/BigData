06a3ffa64ef06ba996bc5f134abd3c0207fe77ed
vhidy <pidimansyahputra@gmail.com>
2018-09-24 04:14:26 -0700

    list as quest

---
+++ minggu-03/praktik/src/usingListAsQuest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.7
+from collections import deque
+queue = deque(["Eric", "John", "Michael"])
+queue.append("Terry")
+queue.append("Graham")
+queue.popleft()
+queue.popleft()
+print(queue)
